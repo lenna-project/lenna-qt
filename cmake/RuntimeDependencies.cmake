44e5f9e088c1cf133f48769c4cb946029059f53b
FalseCAM <falsecam@gmail.com>
2016-06-20 22:55:41 +0200

    Installer updates, Plugins to installer added

--- cmake/RuntimeDependencies.cmake
+++ cmake/RuntimeDependencies.cmake
@@ -15,5 +15,11 @@ if(OPTION_SELF_CONTAINED)
 
     # Install 3rd-party runtime dependencies into runtime-component
     # install(FILES ... COMPONENT runtime)
+	if(WIN32)
+                set(QT_FILES $ENV{QT_DIR}/bin)
+                set(OpenCV_FILES $ENV{OpenCV_DIR}/build/bin/Release)
+                install(FILES "${QT_FILES}/Qt5Core.dll" "${QT_FILES}/Qt5Gui.dll" "${QT_FILES}/Qt5Widgets.dll" "${QT_FILES}/Qt5Network.dll"
+                    "${OpenCV_FILES}/opencv_world310.dll" "${OpenCV_FILES}/opencv_world310.dll" DESTINATION ${INSTALL_BIN} COMPONENT runtime)
+	endif()
 
 endif()
